56c18351b4bc2264285ccc6836d865b03edf5976
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-21 23:57:12 +0530

    [Adarsh] Add. UserDTO model for handling user data

---
+++ SpringMessagingApp/src/main/java/com/example/app/model/UserDTO.java
@@ -0,0 +1,29 @@
+package com.example.app.model;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+    public UserDTO() {}
+
+    public UserDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
